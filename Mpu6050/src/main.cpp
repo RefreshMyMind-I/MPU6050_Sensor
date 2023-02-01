b5acae62a53dfef094e65565f00db3d2f2d292d8
Kirtiraj <refreshmymind0@gmail.com>
2023-02-01 22:21:57 +0530

    added Y and Z axis to the code It will spilt out raw data

--- Mpu6050/src/main.cpp
+++ Mpu6050/src/main.cpp
@@ -1,25 +1,38 @@
-#include "Wire.h" 
+#include "Wire.h"
 #include <Arduino.h>
 #define MPU6050_ADDR 0x68 // Alternatively set AD0 to HIGH  --> Address = 0x69
 
-
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   Wire.begin();
   Wire.beginTransmission(MPU6050_ADDR);
-  Wire.write(0x6B); 
-  Wire.write(0); 
+  Wire.write(0x6B);
+  Wire.write(0);
   Wire.endTransmission();
 }
-void loop() {
+void loop()
+{
   Wire.beginTransmission(MPU6050_ADDR);
   Wire.write(0x3B);
   Wire.endTransmission();
-  Wire.requestFrom(MPU6050_ADDR,14,true);
-  double accX = Wire.read()<<8 | Wire.read();
-  double accXm = map(accX,-17000,17000,-10.0,10.0);
-  Serial.print("AcX = "); Serial.println(accXm);
+  Wire.requestFrom(MPU6050_ADDR, 14, true);
+  double accX = Wire.read() << 8 | Wire.read();
+  Serial.print("AcX = ");
+  Serial.println(accX);
+
+  Wire.write(0x3C);
+  Wire.endTransmission();
+  Wire.requestFrom(MPU6050_ADDR, 14, true);
+  double accX = Wire.read() << 8 | Wire.read();
+  Serial.print("AcY = ");
+  Serial.println(accX);
+
+  Wire.write(0x3D);
+  Wire.endTransmission();
+  Wire.requestFrom(MPU6050_ADDR, 14, true);
+  double accX = Wire.read() << 8 | Wire.read();
+  Serial.print("AcZ = ");
+  Serial.println(accX);
   delay(100);
-
 }
-
